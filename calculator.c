6c52a63c4acce7fb10853db2f7c5012592cc27c8
shivam <24je0693@iitism.ac.in>
2024-10-24 10:49:18 +0530

    inverse implemt

--- calculator.c
+++ calculator.c
@@ -25,6 +25,9 @@ int square(int i){
 int cube(int i){
 	return(i*i*i)
 }
+float inv(int i){
+	return(1/i)
+}
 int main() {
     int a, b;
     char operation;
@@ -79,7 +82,7 @@ int main() {
             case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("result is %d\n",inv(a));
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
